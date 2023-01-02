ac258bb037f68f381cc50903eac80827ebc9ee0f
Ryan Hileman <lunixbochs@gmail.com>
2023-01-01 16:20:21 -0800

    work around a GLXBadWindow panic

--- src/platform_impl/linux/x11/event_processor.rs
+++ src/platform_impl/linux/x11/event_processor.rs
@@ -541,10 +541,9 @@ impl<T: 'static> EventProcessor<T> {
 
                 // Since all XIM stuff needs to happen from the same thread, we destroy the input
                 // context here instead of when dropping the window.
-                wt.ime
+                let _ = wt.ime
                     .borrow_mut()
-                    .remove_context(window)
-                    .expect("Failed to destroy input context");
+                    .remove_context(window);
 
                 callback(Event::WindowEvent {
                     window_id,
